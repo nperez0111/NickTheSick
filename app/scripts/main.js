add814c463e85cfb5ef09a8c62530b4e45e82a7f
Nick Perez <nperez0111@gmail.com>
2015-09-23 23:25:48 -0400

    Added Work section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,14 +3,62 @@
 
 $(document).ready(function () {
     $('#fullpage').fullpage({
-        //anchors: ['Page1', 'Page2', 'Page3', 'Page4'],
-        //menu: '#menu'
-        navigation: true,
-        navigationTooltips: ['Page1', 'Page2', 'Page3', 'Page4'],
-        showActiveTooltip: true,
-        navigationPosition: 'right'
+        anchors: ['Welcome', 'Page2', 'Page3', 'Work'],
+        menu: '#menu',
+        navigation: false,
+        scrollOverflow: true,
+        loopTop: true
+
     });
 });
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//navigationTooltips: ['Page1', 'Page2', 'Page3', 'Page4'],
+//showActiveTooltip: true,
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
 //# sourceMappingURL=main.js.map
 //# sourceMappingURL=main.js.map
 //# sourceMappingURL=main.js.map

debfeee22d818574e1362b1bcad304fc2847cfcf
Nick Perez <nperez0111@gmail.com>
2016-06-06 23:01:23 -0400

    Added NOAA Forecast App

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,17 @@
 // jshint devel:true
 'use strict';
 
-$( document ).ready( function () {
-    $( '#fullpage' ).fullpage( {
-        anchors: [ 'Welcome', 'About', 'Work' ],
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        anchors: ['Welcome', 'About', 'Work'],
         menu: '#menu',
         navigation: false,
         scrollOverflow: true,
         /*//*/
         loopTop: true //*/
 
-    } );
-    $( '.down' ).click( $.fn.fullpage.moveSectionDown );
-    $( '.left' ).click( $.fn.fullpage.moveSlideRight );
-} );
+    });
+    $('.down').click($.fn.fullpage.moveSectionDown);
+    $('.left').click($.fn.fullpage.moveSlideRight);
+});
+//# sourceMappingURL=main.js.map

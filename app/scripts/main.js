587a1553657071246e01cfc313c347fd5f4a76ef
Nick Perez <nperez0111@gmail.com>
2016-06-06 20:38:52 -0400

    add hide-address-bar as dependency

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,60 +1,16 @@
 // jshint devel:true
 'use strict';
 
-$(document).ready(function () {
-    $('#fullpage').fullpage({
-        anchors: ['Welcome', 'About', 'Work'],
+$( document ).ready( function () {
+    $( '#fullpage' ).fullpage( {
+        anchors: [ 'Welcome', 'About', 'Work' ],
         menu: '#menu',
         navigation: false,
         scrollOverflow: true,
-        /*//*/loopTop: true //*/
+        /*//*/
+        loopTop: true //*/
 
-    });
-    $('.down').click($.fn.fullpage.moveSectionDown);
-    $('.left').click($.fn.fullpage.moveSlideRight);
-    if (!window.pageYOffset) {
-        hideAddressBar();
-    }
-});
-
-function hideAddressBar() {
-    if (document.height < window.outerHeight) {
-        document.body.style.height = window.outerHeight + 50 + 'px';
-    }
-
-    setTimeout(function () {
-        window.scrollTo(0, 1);
-    }, 50);
-}
-
-window.addEventListener("orientationchange", hideAddressBar);
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
+    } );
+    $( '.down' ).click( $.fn.fullpage.moveSectionDown );
+    $( '.left' ).click( $.fn.fullpage.moveSlideRight );
+} );

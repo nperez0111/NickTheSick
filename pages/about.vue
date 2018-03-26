03d9b9946b625c38a6c0ac370dabe82986af8140
Nick Perez <nperez0111@gmail.com>
2018-03-25 23:04:18 -0700

    add screenshots and accomadate them

--- pages/about.vue
+++ pages/about.vue
@@ -4,41 +4,58 @@
   		<v-layout column justify-center align-center>
 	  	<h1 :class="{'text-xs-center':true, 'ma-5':$vuetify.breakpoint.lgAndUp, 'ma-3':$vuetify.breakpoint.mdAndDown, 'display-3':$vuetify.breakpoint.lgAndUp, 'display-4':$vuetify.breakpoint.xlAndUp,'display-2':$vuetify.breakpoint.mdAndDown}">About Nick the Sick</h1>
 	  	</v-layout>
-		<v-layout column justify-space-between align-center>
-			<div class="text-editor-wrap">
-				<div ref="typerStrings">
-					<p>Hey thanks for coming to my website! I know there are a lot of other websites you could be on right now and you choose to come here so thanks!</p>
-					<p>So I know how annoying it is to read from this so just click below!</p>
-				</div>
-				<div class="title-bar">
-					<span class="black--text">
-						Nick The Sick — bash — 80x<span class="terminal-height">10</span>
-					</span>
-				</div>
-				<div class="text-body">
-					$ <span ref="typer"></span>
-				</div>
-			</div>
-			<v-btn icon flat fab :class="{'ma-4':$vuetify.breakpoint.mdAndUp,'mb-3':$vuetify.breakpoint.smAndDown}" @click="$vuetify.goTo($refs['page-2'])">
-				<v-icon class="display-3">keyboard_arrow_down</v-icon>
-			</v-btn>
-		</v-layout>
-	</Full-Height>
-  <v-layout class="lg4 fullwidth blue darken-3" ref="page-2">
-  		<v-layout class="md5 flex fullwidth justify-center align-center py-5">
-        <div class="avatar">
-          <img src="https://image.shutterstock.com/mosaic_250/0/0/518740741.jpg">
+  		<v-layout column justify-space-between align-center>
+  			<div class="text-editor-wrap">
+  				<div ref="typerStrings" style="display:none">
+  					<p>Hey thanks for coming to my website! I know there are a lot of other websites you could be on right now and you choose to come here so thanks!</p>
+  					<p>So I know how annoying it is to read from this so just click below!</p>
+  				</div>
+  				<div class="title-bar">
+  					<span class="black--text">
+  						Nick The Sick — bash — 80x<span class="terminal-height">10</span>
+  					</span>
+  				</div>
+  				<div class="text-body">
+  					$ <span ref="typer"></span>
+  				</div>
+  			</div>
+  			<v-btn icon flat fab :class="{'ma-4':$vuetify.breakpoint.mdAndUp,'mb-3':$vuetify.breakpoint.smAndDown}" @click="$vuetify.goTo($refs['page-2'])">
+  				<v-icon class="display-3">keyboard_arrow_down</v-icon>
+  			</v-btn>
+  		</v-layout>
+  	</Full-Height>
+    <v-layout class="fullwidth blue darken-3 wrap py-5" ref="page-2">
+      <v-layout class="xs12 flex fullwidth justify-center align-center py-5">
+        <h1 class="display-2">I'm Nick the Sick A.K.A Nicholas Perez</h1>
+      </v-layout>
+  		<v-layout class="sm12 md5 flex fullwidth justify-center align-center py-5">
+        <div class="avatar my-5">
+          <img src="https://image.shutterstock.com/mosaic_250/0/0/518740741.jpg" class="elevation-1 profile-img">
         </div>
       </v-layout>
-  		<v-layout class="md7 flex fullwidth justify-center align-center">
-        <p>Hi, my name is Nicholas Perez! I make stuff a lot of different types of stuff like websites,web applications, programs.</p>
+  		<v-layout class="sm12 md7 flex fullwidth justify-center align-center py-5 px-5">
+        <p class="headline text-xs-center">I make stuff a lot of different types of stuff like websites, web applications, mobile applications, desktop programs and scripts.</p>
       </v-layout>
   	</v-layout>
-  	<v-layout class="lg4 fullwidth">
-  		<v-layout class="md5 order-md2 flex fullwidth red justify-center align-center">The Work</v-layout>
-  		<v-layout class="md7 flex fullwidth blue justify-center align-center">A</v-layout>
+  	<v-layout class="fullwidth indigo darken-3" wrap>
+      <v-layout class="xs12 flex fullwidth justify-center align-center py-5">
+        <h1 class="display-2">Recent Projects</h1>
+      </v-layout>
+  		<v-layout class="md6 lg4 flex fullwidth justify-center align-center column pt-1 pb-5 px-5" v-for="item in projects" :key="item.image">
+        <img :src="item.image" :alt="item.title + ' user interface image'" :class="{fullwidth:true, 'elevation-1':item.shadow}">
+        <h2 class="mt-1 mb-1 headline text-xs-center" v-text="item.title"></h2>
+        <h3 class="text-xs-center subheading mb-3" v-text="item.description"></h3>
+        <v-btn :href="item.projectLink" v-if="item.projectLink" color="primary">
+          Go see the Project
+          <v-icon right>arrow_forward</v-icon>
+        </v-btn>
+        <v-btn :href="item.viewerLink" v-if="item.viewerLink" color="primary">
+          Go see the Website
+          <v-icon right>arrow_forward</v-icon>
+        </v-btn>
+      </v-layout>
   	</v-layout>
-  	<v-layout class="lg4 fullwidth">
+  	<v-layout class="fullwidth">
   		<v-layout class="md5 flex fullwidth green justify-center align-center">The Legend</v-layout>
   		<v-layout class="md7 flex fullwidth purple justify-center align-center">B</v-layout>
   	</v-layout>
@@ -50,7 +67,23 @@ import Typer from 'typed.js'
 	export default {
 		data(){
 			return {
-				typerInstance:null
+				typerInstance:null,
+        projects:[{
+          image:'/pianoserver.png',
+          title:'Pianoserver',
+          description:'Full integration of Pandora into your computer with Notifications, Shortcuts and a Web UI.',
+          projectLink:'https://github.com/nperez0111/pianoserver'
+        },{
+          image:'/dresscoderules.png',
+          title:'DressCodeRules - Hydra',
+          description:'An internal use tool for DressCodeRules.com to aggregate Product feeds to then be inserted into the WordPress site in the proper format.',
+          projectLink:'https://bitbucket.org/dresscoderules/hydra/overview'
+        },{
+          image:'/lightsource.png',
+          title:'LightSource SF Mounting Calculator',
+          description:'This is an internal use tool for LightSource Sf, a printing company, to be able to accurately price their prints',
+          viewerLink:'http://calc.lightsourcesf.com/'
+        }]
 			}
 		},
 		mounted(){
@@ -172,5 +205,9 @@ import Typer from 'typed.js'
 }
 .fullwidth{
 	width:100%;
+}
+img.profile-img{
+  max-width:325px;
+  width:100%;
 }
 </style>

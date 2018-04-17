5e8c4f153ec1073208147e7e4c538d9460a4ad34
Nick Perez <nperez0111@gmail.com>
2018-04-16 20:31:36 -0700

    change color scheme up

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
                 <h2 class="title text-xs-center mt-3">To create something that makes people wonder how they ever lived without it, is my goal.</h2>
             </v-layout>
             <v-layout column :justify-space-around="$vuetify.breakpoint.mdAndDown" :justify-space-between="$vuetify.breakpoint.lgAndUp" align-center>
-                <div class="text-editor-wrap">
+                <div class="text-editor-wrap supa-shadow">
                     <div ref="typerStrings" style="display:none">
                         <p> a Full Stack Web Developer</p>
                         <p> a Software Engineer</p>
@@ -15,7 +15,7 @@
                         <p> an UI/UX Designer</p>
                     </div>
                     <div class="title-bar">
-                        <span class="black--text">Nick The Sick' Website — bash — 120x20</span>
+                        <span class="black--text">Nick The Sick's Website — bash — 120x20</span>
                     </div>
                     <div class="text-body green--text">
                         <p><span class="white--text">$</span> echo "<span class="white--text">$WELCOME_MESSAGE</span>"</p>
@@ -24,27 +24,24 @@
                         <span class="white--text">I am </span><span ref="typer" class="white--text"></span>
                     </div>
                 </div>
-                <v-btn icon flat fab :class="{'ma-4':$vuetify.breakpoint.mdAndUp,'mb-3':$vuetify.breakpoint.mdAndDown,'lower-hover':true}" @click="$vuetify.goTo($refs['page-2'])">
-                    <v-icon class="display-3">keyboard_arrow_down</v-icon>
-                </v-btn>
             </v-layout>
         </Full-Height>
-        <v-card class="mx-4 pull-up white--text pa-1 blue darken-3" raised ref="page-2">
-        <v-layout class="fullwidth wrap py-5">
-            <v-layout class="xs12 flex fullwidth justify-center align-center pt-3 pb-5">
-                <h1 class="display-2 mx-5 text-xs-center">Who is Nick the Sick?</h1>
+        <v-card :class="{'mx-5':$vuetify.breakpoint.smAndUp, 'pull-up':$vuetify.breakpoint.smAndUp, 'pa-1':$vuetify.breakpoint.smAndUp, grey:true, 'lighten-3':true, 'supa-shadow':true}" ref="page-2">
+            <v-layout class="fullwidth wrap py-5">
+                <v-layout class="xs12 flex fullwidth justify-center align-center pt-3 pb-5">
+                    <h1 class="display-2 mx-5 text-xs-center">Who are you?</h1>
+                </v-layout>
+                <v-layout :class="{sm12:true, md5:true,xl6:true, flex:true, fullwidth:true, 'justify-center':true, 'align-center':true, 'py-5':$vuetify.breakpoint.mdAndUp,'py-2':$vuetify.breakpoint.mdAndDown}">
+                    <div :class="{avatar:true, 'my-5':$vuetify.breakpoint.mdAndUp,'my-2':$vuetify.breakpoint.mdAndDown}">
+                        <img src="https://image.shutterstock.com/mosaic_250/0/0/518740741.jpg" class="elevation-3 profile-img raise-hover">
+                    </div>
+                </v-layout>
+                <v-layout class="sm12 md7 xl5 flex fullwidth justify-center align-center py-5 px-4 column">
+                    <p :class="{headline:$vuetify.breakpoint.mdAndUp,'body-2':$vuetify.breakpoint.smAndDown,bump:true,'text-xs-center':true,'text-md-left':true}">I am a software developer who is passionate about using technology to make people's lives easier. I specialize in front end web apps and websites, but don't let that pigeonhole me as <i>only</i> a web developer. My dream is that one day someone will show me a product raving about how useful it is to them and to be able to reply "I made that".</p>
+                    <p :class="{headline:$vuetify.breakpoint.mdAndUp,'body-2':$vuetify.breakpoint.smAndDown,bump:true,'text-xs-center':true,'text-md-left':true}">I am a person who wholeheartedly enjoys creating software, I love the idea that I can make something which simply did not exist and could not have existed without me. I have a knack for learning new techniques and have great pride in my work so I try to deliver the best that I can. I grew up in Miami where there was not much of a "Tech" scene, so I moved to San Francisco to hopefully be able to be apart of the creation of great software. </p>
+                </v-layout>
             </v-layout>
-            <v-layout :class="{sm12:true, md5:true, flex:true, fullwidth:true, 'justify-center':true, 'align-center':true, 'py-5':$vuetify.breakpoint.mdAndUp,'py-2':$vuetify.breakpoint.mdAndDown}">
-                <div :class="{avatar:true, 'my-5':$vuetify.breakpoint.mdAndUp,'my-2':$vuetify.breakpoint.mdAndDown}">
-                    <img src="https://image.shutterstock.com/mosaic_250/0/0/518740741.jpg" class="elevation-3 profile-img raise-hover">
-                </div>
-            </v-layout>
-            <v-layout class="sm12 md7 flex fullwidth justify-center align-center py-5 px-4 column">
-                <p :class="{headline:$vuetify.breakpoint.mdAndUp,'body-2':$vuetify.breakpoint.smAndDown,bump:true,'text-xs-center':true,'text-md-left':true}">I am a software developer who is passionate about using technology to make people's lives easier. I specialize in front end web apps and websites, but don't let that pigeonhole me as <i>only</i> a web developer. My dream is that one day someone will show me a product raving about how useful it is to them and to be able to reply "I made that".</p>
-                <p :class="{headline:$vuetify.breakpoint.mdAndUp,'body-2':$vuetify.breakpoint.smAndDown,bump:true,'text-xs-center':true,'text-md-left':true}">I am a person who wholeheartedly enjoys creating software, I love the idea that I can make something which simply did not exist and could not have existed without me. I have a knack for learning new techniques and have great pride in my work so I try to deliver the best that I can. I grew up in Miami where there was not much of a "Tech" scene, so I moved to San Francisco to hopefully be able to be apart of the creation of great software. </p>
-            </v-layout>
-        </v-layout>
-    </v-card>
+        </v-card>
         <v-layout class="fullwidth indigo darken-3 justify-center align-center flat white--text" wrap>
             <v-layout class="xs12 flex fullwidth justify-center align-center py-5">
                 <h1 class="display-2 mx-5 text-xs-center">Recent Projects</h1>
@@ -63,71 +60,71 @@
                 </v-btn>
             </v-layout>
         </v-layout>
-        <v-card class="mx-4 pull-up mt-5 mb-4 blue-grey lighten-3" raised>
-        <v-layout class="fullwidth pb-5" wrap>
-            <v-layout class="xs12 flex fullwidth justify-center align-center py-5">
-                <h1 class="display-2 mx-5 text-xs-center">Contact Me</h1>
-            </v-layout>
-            <v-layout class="xs12 md7 justify-center align-center flex fullwidth px-5 mb-5" wrap>
-                <v-form v-model="form.valid" class="fullwidth" ref="form" lazy-validation>
-                    <v-text-field label="Name" v-model="form.name" required :rules="[v => !!v || 'Name is required']" class="my-2"></v-text-field>
-                    <v-text-field label="E-mail" v-model="form.email" :rules="form.emailRules" required class="my-2"></v-text-field>
-                    <v-text-field label="Message" v-model="form.message" textarea required :rules="[v => !!v || 'Message is required']"></v-text-field>
-                    <v-layout>
-                        <v-spacer></v-spacer>
-                        <v-btn color="primary white--text" @click="sendMessage" :loading="sendingMessage" :disabled="sendingMessage" class="raise-hover">Send Message
-                            <v-icon right color="green lighten-2">send</v-icon>
-                        </v-btn>
+        <v-card :class="{'mx-4':$vuetify.breakpoint.smAndUp, 'mt-5':true, 'mb-4':true, grey:true, 'lighten-3':true, fullwidth:$vuetify.breakpoint.xs,'supa-shadow':true}">
+            <v-layout class="fullwidth pb-5" wrap>
+                <v-layout class="xs12 flex fullwidth justify-center align-center py-5">
+                    <h1 class="display-2 mx-5 text-xs-center">Contact Me</h1>
+                </v-layout>
+                <v-layout :class="{xs12:true, md7:true, 'justify-center':true, 'align-center':true, flex:true, fullwidth:true, 'px-5':$vuetify.breakpoint.smAndUp,'px-2':$vuetify.breakpoint.xsAndUp, 'mb-5':true}" wrap>
+                    <v-form v-model="form.valid" class="xs11 flex" ref="form" lazy-validation>
+                        <v-text-field label="Name" v-model="form.name" required :rules="[v => !!v || 'Name is required']" class="my-2"></v-text-field>
+                        <v-text-field label="E-mail" v-model="form.email" :rules="form.emailRules" required class="my-2"></v-text-field>
+                        <v-text-field label="Message" v-model="form.message" textarea required :rules="[v => !!v || 'Message is required']"></v-text-field>
+                        <v-layout>
+                            <v-spacer></v-spacer>
+                            <v-btn color="primary white--text" @click="sendMessage" :loading="sendingMessage" :disabled="sendingMessage" class="raise-hover">Send Message
+                                <v-icon right color="green lighten-2">send</v-icon>
+                            </v-btn>
+                        </v-layout>
+                    </v-form>
+                </v-layout>
+                <v-layout :class="{xs12:true, md5:true, 'justify-center':true, 'align-center':true, flex:true, column:true, 'pt-1':true, 'pb-5':true, 'mb-3':true, 'px-5':$vuetify.breakpoint.smAndUp,hotLinks:true}">
+                    <v-layout class="flex md6 fullwidth">
+                        <v-card class="flex md6 layout column justify-center align-center ma-2 text-xs-center fullwidth raise-hover grey darken-4 white--text" href="tel:3052828358" hover ripple>
+                            <v-card-media class="flex xs12 mt-3">
+                                <v-icon class="icon-phone-squared display-3 white--text"></v-icon>
+                            </v-card-media>
+                            <v-card-text>
+                                <h2>Phone</h2>
+                                <p>(305)-282-8358</p>
+                            </v-card-text>
+                        </v-card>
+                        <v-card class="flex md6 layout column justify-center align-center ma-2 text-xs-center fullwidth raise-hover grey darken-4 white--text" href="mailto:nperez0111@gmail.com" hover ripple>
+                            <v-card-media class="flex xs12 mt-3">
+                                <v-icon class="icon-mail-squared display-3 white--text"></v-icon>
+                            </v-card-media>
+                            <v-card-text>
+                                <h2>Email</h2>
+                                <p>nperez0111@gmail.com</p>
+                            </v-card-text>
+                        </v-card>
                     </v-layout>
-                </v-form>
-            </v-layout>
-            <v-layout class="xs12 md5 justify-center align-center flex column pt-1 pb-5 mb-3 px-5">
-                <v-layout class="flex md6 fullwidth">
-                    <v-card class="flex md6 layout column justify-center align-center ma-2 text-xs-center fullwidth raise-hover grey darken-4 white--text" href="tel:3052828358" hover ripple>
-                        <v-card-media class="flex xs12 mt-3">
-                            <v-icon class="icon-phone-squared display-3 white--text"></v-icon>
-                        </v-card-media>
-                        <v-card-text>
-                            <h2>Phone</h2>
-                            <p>(305)-282-8358</p>
-                        </v-card-text>
-                    </v-card>
-                    <v-card class="flex md6 layout column justify-center align-center ma-2 text-xs-center fullwidth raise-hover grey darken-4 white--text" href="mailto:nperez0111@gmail.com" hover ripple>
-                        <v-card-media class="flex xs12 mt-3">
-                            <v-icon class="icon-mail-squared display-3 white--text"></v-icon>
-                        </v-card-media>
-                        <v-card-text>
-                            <h2>Email</h2>
-                            <p>nperez0111@gmail.com</p>
-                        </v-card-text>
-                    </v-card>
+                    <v-layout class="flex md6 fullwidth">
+                        <v-card class="flex md6 layout column justify-center align-center ma-2 text-xs-center fullwidth raise-hover grey darken-4 white--text" href="https://github.com/nperez0111" hover ripple>
+                            <v-card-media class="flex xs12 mt-3">
+                                <v-icon class="icon-github-squared display-3 white--text"></v-icon>
+                            </v-card-media>
+                            <v-card-text>
+                                <h2>Github</h2>
+                                <p>@nperez0111</p>
+                            </v-card-text>
+                        </v-card>
+                        <v-card class="flex md6 layout column justify-center align-center ma-2 text-xs-center fullwidth raise-hover grey darken-4 white--text" href="https://www.linkedin.com/in/nicholas-perez-215262124" hover ripple>
+                            <v-card-media class="flex xs12 mt-3">
+                                <v-icon class="icon-linkedin-squared display-3 white--text"></v-icon>
+                            </v-card-media>
+                            <v-card-text>
+                                <h2>Linkedin</h2>
+                                <p>Click here</p>
+                            </v-card-text>
+                        </v-card>
+                    </v-layout>
                 </v-layout>
-                <v-layout class="flex md6 fullwidth">
-                    <v-card class="flex md6 layout column justify-center align-center ma-2 text-xs-center fullwidth raise-hover grey darken-4 white--text" href="https://github.com/nperez0111" hover ripple>
-                        <v-card-media class="flex xs12 mt-3">
-                            <v-icon class="icon-github-squared display-3 white--text"></v-icon>
-                        </v-card-media>
-                        <v-card-text>
-                            <h2>Github</h2>
-                            <p>@nperez0111</p>
-                        </v-card-text>
-                    </v-card>
-                    <v-card class="flex md6 layout column justify-center align-center ma-2 text-xs-center fullwidth raise-hover grey darken-4 white--text" href="https://www.linkedin.com/in/nicholas-perez-215262124" hover ripple>
-                        <v-card-media class="flex xs12 mt-3">
-                            <v-icon class="icon-linkedin-squared display-3 white--text"></v-icon>
-                        </v-card-media>
-                        <v-card-text>
-                            <h2>Linkedin</h2>
-                            <p>Click here</p>
-                        </v-card-text>
-                    </v-card>
+                <v-layout class="xs12 justify-center align-center headline">
+                    Made by:&nbsp;<span class="blue--text">Nick the Sick</span>
                 </v-layout>
             </v-layout>
-            <v-layout class="xs12 justify-center align-center headline">
-                Made by:&nbsp;<span class="black--text">Nick the Sick</span>
-            </v-layout>
-        </v-layout>
-    </v-card>
+        </v-card>
     </v-layout>
 </template>
 <script>
@@ -246,14 +243,17 @@ export default {
     background-size: cover;
 }
 
+.supa-shadow {
+    box-shadow: rgba(0, 0, 0, 0.8) 0px 20px 70px;
+}
+
 .text-editor-wrap {
     display: block;
     margin: auto;
     width: 90%;
     max-width: 840px;
-    margin: 15px;
+    margin: 15px 0;
     border-radius: 10px;
-    box-shadow: rgba(0, 0, 0, 0.8) 0px 20px 70px;
     clear: both;
     overflow: hidden;
     -webkit-transition: all 0.5s ease-out;
@@ -370,7 +370,7 @@ export default {
 img.profile-img {
     max-width: 325px;
     width: 100%;
-    border:2px solid #FFF;
+    border: 2px solid #FFF;
 }
 
 input:-webkit-autofill,
@@ -388,17 +388,30 @@ select:-webkit-autofill:focus {
 .bump {
     font-size: 125% !important;
 }
-.raise-hover, .lower-hover{
-    transition:all 0.5s;
+
+.raise-hover,
+.lower-hover {
+    transition: all 0.5s;
 }
-.raise-hover:hover{
-    transform:translateY(-8px);
+
+.raise-hover:hover {
+    transform: translateY(-8px);
 }
-.lower-hover:hover{
-    transform:translateY(6px);
+
+.lower-hover:hover {
+    transform: translateY(6px);
 }
-.pull-up{
-    position:relative;
-    top:-16px;
+
+.pull-up {
+    position: relative;
+    top: -40px;
+}
+
+.parallax__content {
+    padding: 0 !important;
+}
+
+.hotLinks .card__text {
+    padding: 10px 0 !important;
 }
 </style>
